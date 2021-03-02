8f1f9b80e30fc942a0417995c0e58b2fa3a048ee
devNicku <stsnicky@gmail.com>
2021-03-02 00:59:07 -0800

    resortApp

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/resort_db', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then(() => console.log('connected!'))
+//     .catch(error => console.log(error));
